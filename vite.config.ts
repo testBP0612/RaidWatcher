c20a67d005567817685f7d64598de50db40f5a71
Bloop <sp112131@gmail.com>
2025-03-30 23:31:01 +0800

    rename project from Lazyraiders to RaidWatcher

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: '/lazyraiders/', // 必須 GitHub 倉庫名稱一致
+  base: '/RaidWatcher/', // 必須 GitHub 倉庫名稱一致
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
